71b867f1d3fbc433aed18a404521aea0e15688c4
dsad <sswdarius@gmail.com>
2025-07-06 21:56:33 +0300

    kkkkkkkk

--- src/app/TriviaGame.js
+++ src/app/TriviaGame.js
@@ -247,9 +247,9 @@ const twitterShareLink = `https://twitter.com/intent/tweet?text=${encodeURICompo
         )}
       </div>
 
-      <div
+    <div
   className="mt-6 text-center text-sm text-gray-600 cursor-pointer hover:text-blue-600 transition select-none"
   onClick={() => window.open('https://x.com/abigrafikvarmi', '_blank')}
 >
-  dsad
+  {nickname || 'dsad'}
 </div>
